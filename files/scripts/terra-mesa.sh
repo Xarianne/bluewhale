63dfeb97a7da79a61908885fe742ab47833ac42f
Xariann <130084241+Xarianne@users.noreply.github.com>
2026-04-19 15:12:45 +0100

    Update terra-mesa.sh to include mesa drivers
    Added installation of mesa-related packages to the script.

--- files/scripts/terra-mesa.sh
+++ files/scripts/terra-mesa.sh
@@ -4,8 +4,8 @@ set -eoux pipefail
 # 1. Enable Terra Repos (Mesa & Multimedia are key for your RX 9070)
 dnf5 install -y --refresh --nogpgcheck --repofrompath "terra,https://repos.fyralabs.com/terra$(rpm -E %fedora)" \
   terra-release \
-  terra-release-multimedia
-  # terra-release-mesa \
+  terra-release-multimedia \
+  terra-release-mesa 
 
 # 2. Mirroring uBlue Overrides (Adjusted for Terra 44 Names)
 # We remove the '-freeworld' suffixes and target the base names
@@ -16,13 +16,13 @@ dnf5 install -y --allowerasing --skip-unavailable \
     gstreamer1-plugins-bad \
     gstreamer1-plugins-ugly \
     gstreamer1-vaapi \
-    gstreamer1-plugin-libav
-    # mesa-va-drivers \
-    # mesa-vulkan-drivers \
-    # mesa-dri-drivers \
-    # mesa-libgbm \
-    # mesa-libEGL \
-    # mesa-libGL \
+    gstreamer1-plugin-libav \
+    mesa-va-drivers \
+    mesa-vulkan-drivers \
+    mesa-dri-drivers \
+    mesa-libgbm \
+    mesa-libEGL \
+    mesa-libGL
 
 # 3. Cleanup
 dnf5 clean all
