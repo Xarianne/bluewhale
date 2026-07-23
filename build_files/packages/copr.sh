3dc404a559824248e2b1df6c734e88254bafcbe6
xarianne <xariann.widely103@passmail.com>
2026-07-23 10:28:17 +0100

    Moving Better Blur to Nix

--- build_files/packages/copr.sh
+++ build_files/packages/copr.sh
@@ -16,8 +16,3 @@ dnf5 -y copr enable deltacopy/darkly
 dnf5 install -y \
     darkly
 dnf5 -y copr disable deltacopy/darkly
-
-dnf5 -y copr enable infinality/kwin-effects-better-blur-dx
-dnf5 install -y \
-    kwin-effects-better-blur-dx
-dnf5 -y copr disable infinality/kwin-effects-better-blur-dx
