0c2e816044b7cdae95a825bcc8325654d2155671
xarianne <xariann.widely103@passmail.com>
2026-07-28 07:51:04 +0100

    Added Hyprland with Dank Material Shell

--- build_files/packages/copr.sh
+++ build_files/packages/copr.sh
@@ -21,3 +21,14 @@ dnf5 -y copr enable infinality/kwin-effects-better-blur-dx
 dnf5 install -y \
     kwin-effects-better-blur-dx
 dnf5 -y copr disable infinality/kwin-effects-better-blur-dx
+
+# Hyprland with Dank Material Shell
+dnf5 -y copr enable lionheartp/Hyprland
+dnf5 install -y \
+    hyprland
+dnf5 -y copr disable lionheartp/Hyprland
+
+dnf5 -y copr enable avengemedia/dms
+dnf5 install -y \
+    dms
+dnf5 -y copr disable avengemedia/dms
